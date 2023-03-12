b34d0aa2e929bd32f56e50f6272a30d4a30aa2e2
Florian Fenna <florian.fenna@proton.me>
2023-03-12 14:09:34 +0100

    Add Function to control visibility of (1) Prüfung

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const currentDate = new Date();
 document.querySelector("#Datum").innerHTML = currentDate.toLocaleDateString();
 
-// Toggle Visibily
+// Toggle Visibily Fiktionsbescheinigung
 const chboxTitel = document.querySelector("#bestaetigungTitel");
 document.querySelectorAll('input[type=radio][name="Fiktionsbescheinigung"]').forEach(function(radio) {
     radio.addEventListener('change', function() {
@@ -13,7 +13,7 @@ document.querySelectorAll('input[type=radio][name="Fiktionsbescheinigung"]').for
     });
   });
 
-// Toggle Visibily Ausschlussgrunde
+//Textblöcke unter Prüfverlauf Dritt (1) werden ausgeblendet, wenn Auswahl in der paragraphen liste
 const ausschlussTextBlock = document.querySelector("#ausschlussGruendeBlock");
 const btnAusschlussGruendeBlock = document.querySelector("#btnAusschlussGruendeBlock");
 document.querySelectorAll('#paragrListe input[type=radio]').forEach(function(radio) {
@@ -28,3 +28,19 @@ document.querySelectorAll('#paragrListe input[type=radio]').forEach(function(rad
     this.classList.add("d-none");
   });
   
+//Wenn ein Textblock unter Prüfverlauf Dritt (1) ausgewählt wird, werden alle anderen Auswahlmöglichkeiten ausgeblendet, bis auf den ausgewählten text block
+function hideTextBlöcke(){
+  const Blöcke = document.querySelectorAll("#ausschlussGruendeBlock .row");
+  Blöcke.forEach(function(block){
+  block.classList.add("d-none");
+  if (block.querySelector("input[type=radio]").checked){
+    block.classList.remove("d-none");
+  };
+  });
+};
+document.querySelectorAll("#ausschlussGruendeBlock input[type=radio]").forEach(function(radio){
+  radio.addEventListener('click', function(){
+    hideTextBlöcke();
+    document.querySelector('#paragrListe').classList.add('d-none');
+  });
+});
