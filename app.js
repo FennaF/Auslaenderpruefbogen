67cd318a2ee9919e74753a0aa68537b6a5880734
Florian Fenna <florian.fenna@proton.me>
2023-03-15 22:43:26 +0100

    Expand Functionality

--- app.js
+++ app.js
@@ -26,25 +26,58 @@ const ausschlussGruendeBlockRadios = document.querySelectorAll("#ausschlussGruen
             ausschlussTextBlock.classList.add("d-none");
 
             /**restlichen Prüfverläufe ausblenden */
-            document.getElementById("PrüfverlaufDritt1.2").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt12").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt21").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt221").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt222").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt223").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt224").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt225").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt226").classList.add("d-none");
 
             btnAusschlussGruendeBlock.classList.remove("d-none");
 
             /**Endergebnis einblenden */
 
-
         });
       });
 
+      //Button um die versteckten Elemente wieder einzublenden
       btnAusschlussGruendeBlock.addEventListener('click', function(event){
         ausschlussTextBlock.classList.remove("d-none");
         paragListe.classList.remove("d-none");
-        showTextblöckeUndPraragrListe();
+        showTextblöcke();
         this.classList.add("d-none");
       });
       
     //Ausblenden paragraphen Liste und Textblöcke, wenn Auswahl eines Textblocks, ausgewählten Texblock einblenden + reset Button + Ende der Prüfung
     //Wenn "Keiner der unter (1) genannten Titel/Status" ausgewählt wird -> alles bleibt eingeblendet -> nächster Prüfblock öffnet sich
+    ausschlussGruendeBlockRadios.forEach(function(radio){
+      radio.addEventListener('click', function(){
+        if(radio.value !== "PrüfDritt1"){
+          hideTextBlöcke();
+          paragListe.classList.add('d-none');
+
+            //restlichen Prüfverläufe ausblenden 
+            document.getElementById("PrüfverlaufDritt12").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt21").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt221").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt222").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt223").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt224").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt225").classList.add("d-none");
+            document.getElementById("PrüfverlaufDritt226").classList.add("d-none");
+
+          /**Endergebnis einblenden */
+
+        } else{
+          document.querySelector("#PrüfverlaufDritt12").classList.remove("d-none");
+          //einzelnen Elemente aus den restlichen Prüfschritten wieder einblenden, sicherhaltshalber, falls die mal ausgeblendet wurden
+          showTextblöcke12();
+        }
+      });
+    });
+
     function hideTextBlöcke(){
       const Blöcke = document.querySelectorAll("#ausschlussGruendeBlock .row");
       Blöcke.forEach(function(block){
@@ -56,62 +89,102 @@ const ausschlussGruendeBlockRadios = document.querySelectorAll("#ausschlussGruen
         });
     };
 
-    function showTextblöckeUndPraragrListe(){
+    function showTextblöcke(){
       const Blöcke = document.querySelectorAll("#ausschlussGruendeBlock .row");
       Blöcke.forEach(function(block){
         block.classList.remove("d-none");
       });
     };
 
-    ausschlussGruendeBlockRadios.forEach(function(radio){
-      radio.addEventListener('click', function(){
-        if(radio.value !== "PrüfDritt1"){
-          hideTextBlöcke();
-          paragListe.classList.add('d-none');
 
-          /**Endergebnis einblenden */
-
-        } else{
-          console.log("Prüfung dooferweise nicht zu Ende. WEITER GEHTS, YIPII!")
-          document.getElementById("PrüfverlaufDritt12").classList.remove("d-none");
-        }
-        /*
-        if(radio.value !== "PrüfDritt1"){
-          console.log("funktioniert");
-        }else{
-          console.log("scheiße")}
-        */
-      });
-    });
-
-    //Prfüng 2
-    function hideTextBlöcke2(){
-      const Blöcke = document.querySelectorAll("#PrüfverlaufDritt12 .row");
-      Blöcke.forEach(function(block){
-        block.classList.add("d-none");
-        if (block.querySelector("input[type=radio]").checked){
-          block.classList.remove("d-none");
-          };
-        });
-    };
-
+    //Prfüng 12
     document.querySelectorAll("#PrüfverlaufDritt12 input[type=radio]").forEach(function(radio){
       radio.addEventListener('click', function(){
         if(radio.value !== "weiter"){
-          hideTextBlöcke2();
+          hideTextBlöcke12();
+          //cheat, sonst wird die erste row immer ausgeblendet, nerviger driss hier
+          if(radio.parentElement.parentElement.classList.contains("col")){
+            document.querySelector("#PrüfverlaufDritt12 .row").classList.remove("d-none");
+          };
+          document.querySelector("#btnPrüfverlaufDritt12").classList.remove("d-none");
+          //restlichen Prüfverläufe ausblenden 
+          document.getElementById("PrüfverlaufDritt21").classList.add("d-none");
           document.getElementById("PrüfverlaufDritt221").classList.add("d-none");
+          document.getElementById("PrüfverlaufDritt222").classList.add("d-none");
+          document.getElementById("PrüfverlaufDritt223").classList.add("d-none");
+          document.getElementById("PrüfverlaufDritt224").classList.add("d-none");
+          document.getElementById("PrüfverlaufDritt225").classList.add("d-none");
+          document.getElementById("PrüfverlaufDritt226").classList.add("d-none");
           /**Endergebnis einblenden */
 
         } else{
-          console.log("Prüfung dooferweise auch hier nicht zu Ende. WEITER GEHTS, VAMOS!")
-          document.getElementById("PrüfverlaufDritt221").classList.remove("d-none");
+          document.getElementById("PrüfverlaufDritt21").classList.remove("d-none");
+          showTextblöcke12();
+          showTextBlöcke21();
         }
-        /*
-        if(radio.value !== "PrüfDritt1"){
-          console.log("funktioniert");
-        }else{
-          console.log("scheiße")}
-        */
       });
     });
 
+    function hideTextBlöcke12(){
+      const Blöcke = document.querySelectorAll("#PrüfverlaufDritt12 .row");
+      Blöcke.forEach(function(block){
+          if(!block.querySelector("input[type]").checked){
+            block.classList.add("d-none");
+           };
+    });
+  };
+
+    function showTextblöcke12(){
+      const Blöcke = document.querySelectorAll("#PrüfverlaufDritt12 .row");
+      Blöcke.forEach(function(block){
+        block.classList.remove("d-none");
+      });
+    };
+
+          //Button um die versteckten Elemente wieder einzublenden
+          document.querySelector("#btnPrüfverlaufDritt12").addEventListener('click', function(event){
+            showTextblöcke12();
+            this.classList.add("d-none");
+          });
+
+
+
+    //Prüfung 21
+        //bei klick ->funktion die alle nicht ja geklickten rows ausblende
+        document.querySelectorAll("#PrüfverlaufDritt21Block input[type=radio]").forEach(function(radio){
+          radio.addEventListener('click', function(){
+            if(radio.value === "Ja"){
+              hideTextBlöcke21();
+            };
+          });
+        });
+
+        function hideTextBlöcke21(){
+          const blöcke = document.querySelectorAll("#PrüfverlaufDritt21Block .row");
+            blöcke.forEach(function(block){
+              let Status = true;
+              block.querySelectorAll("input[type=radio]").forEach(function(radio){
+                if(radio.checked && radio.value === "Ja"){
+                  Status = false;
+                };
+                if(Status){
+                  block.classList.add("d-none");
+                };
+              });
+            });
+            document.querySelector("#btnPrüfverlaufDritt21").classList.remove("d-none")
+        };
+
+        function showTextBlöcke21(){
+          const blöcke = document.querySelectorAll("#PrüfverlaufDritt21Block .row");
+          blöcke.forEach(function(block){
+
+            block.classList.remove("d-none");
+          });
+        }
+
+                  //Button um die versteckten Elemente wieder einzublenden
+                  document.querySelector("#btnPrüfverlaufDritt21").addEventListener('click', function(event){
+                    showTextBlöcke21();
+                    this.classList.add("d-none");
+                  });
