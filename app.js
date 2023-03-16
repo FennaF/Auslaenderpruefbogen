f9b37e64539083fab21b149d47f73375585b8b45
Florian Fenna <florian.fenna@proton.me>
2023-03-16 23:48:50 +0100

    Expand functionality

--- app.js
+++ app.js
@@ -150,11 +150,22 @@ const ausschlussGruendeBlockRadios = document.querySelectorAll("#ausschlussGruen
 
 
     //Prüfung 21
-        //bei klick ->funktion die alle nicht ja geklickten rows ausblende
+        //bei klick auf radio buttons ->funktion die alle rows ausblendet in denen der radio button nicht auf Ja steht
         document.querySelectorAll("#PrüfverlaufDritt21Block input[type=radio]").forEach(function(radio){
           radio.addEventListener('click', function(){
             if(radio.value === "Ja"){
               hideTextBlöcke21();
+              checkbox21.checked = false;
+              document.getElementById("PrüfverlaufDritt21chkb").classList.add("d-none");
+
+              document.getElementById("PrüfverlaufDritt221").classList.add("d-none");
+              document.getElementById("PrüfverlaufDritt222").classList.add("d-none");
+              document.getElementById("PrüfverlaufDritt223").classList.add("d-none");
+              document.getElementById("PrüfverlaufDritt224").classList.add("d-none");
+              document.getElementById("PrüfverlaufDritt225").classList.add("d-none");
+              document.getElementById("PrüfverlaufDritt226").classList.add("d-none");
+            }else{
+              showTextBlöcke21();
             };
           });
         });
@@ -172,19 +183,95 @@ const ausschlussGruendeBlockRadios = document.querySelectorAll("#ausschlussGruen
                 };
               });
             });
-            document.querySelector("#btnPrüfverlaufDritt21").classList.remove("d-none")
+            document.querySelector("#btnPrüfverlaufDritt21").classList.remove("d-none");
         };
 
         function showTextBlöcke21(){
           const blöcke = document.querySelectorAll("#PrüfverlaufDritt21Block .row");
           blöcke.forEach(function(block){
-
             block.classList.remove("d-none");
+            document.querySelector("#btnPrüfverlaufDritt21").classList.add("d-none");
           });
         }
 
                   //Button um die versteckten Elemente wieder einzublenden
                   document.querySelector("#btnPrüfverlaufDritt21").addEventListener('click', function(event){
                     showTextBlöcke21();
+                    document.querySelector("#PrüfverlaufDritt21chkb").classList.remove("d-none");
                     this.classList.add("d-none");
                   });
+          //bei Auswahl des letzten Elements in dem Prüfblock, welches als einziges eine checkbox ist 
+          //-> alle rows des Prüfblocks 2.1 einblenden und alle radio buttons in auf Nein setzten und nächsten Prüfschritt einblenden
+          const checkbox21 = document.querySelector("#PrüfverlaufDritt21Block9")
+          checkbox21.addEventListener("click", function(){
+            if(checkbox21.checked){
+              showTextBlöcke21();
+              setRadios21ToNein();
+              document.querySelector("#PrüfverlaufDritt221").classList.remove("d-none");
+            };
+            console.log(checkbox21.checked);
+          });
+
+          function setRadios21ToNein(){
+            const blöcke = document.querySelectorAll("#PrüfverlaufDritt21Block .row");
+            blöcke.forEach(function(block){
+              block.querySelectorAll("input[type=radio]").forEach(function(radio){
+                if(radio.value === ""){
+                  radio.checked = true;
+                };
+              });
+            });
+          };
+
+      //Prüfung221
+      const PrüfverlaufDritt221BlockARadios = document.querySelectorAll("#PrüfverlaufDritt221BlockA input[type=radio]")
+      const PrüfverlaufDritt221BlockA = document.querySelector("#PrüfverlaufDritt221BlockA")
+      const PrüfverlaufDritt221BlockB = document.querySelector("#PrüfverlaufDritt221BlockB")
+      const btnEinblenden221 = document.querySelector("#btnPrüfverlaufDritt221")
+
+      const PrüfverlaufDritt222 = document.getElementById("PrüfverlaufDritt222")
+      const PrüfverlaufDritt223 = document.getElementById("PrüfverlaufDritt223")
+      const PrüfverlaufDritt224 = document.getElementById("PrüfverlaufDritt224")
+      const PrüfverlaufDritt225 = document.getElementById("PrüfverlaufDritt225")
+      const PrüfverlaufDritt226 = document.getElementById("PrüfverlaufDritt226")
+
+      PrüfverlaufDritt221BlockARadios.forEach(function(radio){
+        radio.addEventListener("click", function(){
+          console.log("klappt");
+          if(radio.value === "Ja"){
+            PrüfverlaufDritt221BlockB.classList.add("d-none");
+            btnEinblenden221.classList.remove("d-none");
+            PrüfverlaufDritt222.classList.remove("d-none");
+
+            PrüfverlaufDritt223.classList.add("d-none")
+            PrüfverlaufDritt224.classList.add("d-none")
+            PrüfverlaufDritt225.classList.add("d-none")
+            PrüfverlaufDritt226.classList.add("d-none")
+
+          } else{
+            PrüfverlaufDritt221BlockB.classList.remove("d-none");
+            btnEinblenden221.classList.add("d-none");
+
+            PrüfverlaufDritt222.classList.add("d-none")
+            PrüfverlaufDritt223.classList.add("d-none")
+            PrüfverlaufDritt224.classList.add("d-none")
+            PrüfverlaufDritt225.classList.add("d-none")
+            PrüfverlaufDritt226.classList.add("d-none")
+          }
+        });
+      });
+
+        //Button um die versteckten Elemente wieder einzublenden
+        btnEinblenden221.addEventListener("click", function(){
+          PrüfverlaufDritt221BlockA.classList.remove("d-none")
+          PrüfverlaufDritt221BlockB.classList.remove("d-none")
+          //showRows221()
+
+          this.classList.add("d-none")
+        })
+
+
+      
+      
+      
+
